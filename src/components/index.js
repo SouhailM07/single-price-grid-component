acc84209ea8f777de2e245e12e139f22685e366d
shadow <lightshadow416@gmail.com>
2023-10-03 18:47:52 +0100

    [WhyUs] component was created

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Container } from "./Container/Container";
 export { default as JoinUs } from "./JoinUs/JoinUs";
 export { default as MonthSub } from "./MonthSub/MonthSub";
+export { default as WhyUs } from "./WhyUs/WhyUs";
